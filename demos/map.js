9a6085670c31369ad56072aff6bdcf3590ab5fdf
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-29 13:29:11 -0400

    initial commit

---
+++ demos/map.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let kids = [
+  { first: "Natalie", last: "Plyers" },
+  { first: "Brittany", last: "Ray" },
+  { first: "Zachary", last: "Westly" },
+];
+
+
+kids.map(function(){
+
+})
